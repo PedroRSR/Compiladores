fee5880ca5e02ee6be838bd73ef3dfff99be07ad
Pedro Ramos Sousa Reis <pedro.rsr0308@gmail.com>
2025-05-18 22:06:28 -0300

    Adição de outros cálculos básicos

---
+++ Calculos.py
@@ -0,0 +1,13 @@
+x = int(input("Digite o primeiro número: "))
+y = int(input("Digite o segundo número: "))
+
+soma = x + y
+subtracao = x - y
+multiplicacao = x * y
+divisao = x / y
+
+print(f"A soma de {x} e {y} é: {soma}")
+print(f"A subtração de {x} e {y} é: {subtracao}")
+print(f"A multiplicação de {x} e {y} é: {multiplicacao}")
+print(f"A divisão de {x} e {y} é: {divisao}")
+# # Esse programa realiza operações matemáticas básicas (soma, subtração, multiplicação e divisão) entre dois números inteiros fornecidos pelo usuário. Ele imprime os resultados de cada operação no console.
--- Hello_World.py
+++ Hello_World.py
@@ -1,2 +1,2 @@
 print("Hello, World!")
-# This is a simple Python program that prints "Hello, World!" to the console.
+# Esse é um programa simples que imprime "Hello, World!" na tela.
